9ee252b1b02c1aaa61f4b0bb89be40ec440ab291
xumpocmb <x.xumpocmb@gmail.com>
2025-02-13 16:58:21 +0300

    app_home models

---
+++ app_home/models.py
@@ -0,0 +1,65 @@
+from django.db import models
+
+
+class SocialLink(models.Model):
+    link_name = models.CharField(max_length=100, blank=True, null=True, verbose_name="Название соц. сети")
+    link_url = models.CharField(max_length=100, blank=True, null=True, verbose_name="Ссылка на соц. сеть")
+
+    class Meta:
+        db_table = 'social_link'
+        verbose_name = 'Ссылка на соц. сеть'
+        verbose_name_plural = 'Ссылки на соц. сети'
+
+    def __str__(self):
+        return f"Ссылка: {self.link_name}"
+
+
+class PhoneNumber(models.Model):
+    phone_name = models.CharField(max_length=100, blank=True, null=True, verbose_name="Название номера")
+    phone_number = models.CharField(max_length=17, blank=True, null=True, verbose_name="Номер телефона")
+
+    class Meta:
+        db_table = 'phone_number'
+        verbose_name = 'Номер телефона'
+        verbose_name_plural = 'Номера телефонов'
+
+    def __str__(self):
+        return f"Номер: {self.phone_name}"
+
+
+class SiteEmail(models.Model):
+    email_name = models.CharField(max_length=50, blank=True, null=True, verbose_name="Название почты")
+    email_address = models.CharField(max_length=50, blank=True, null=True, verbose_name="Адрес почты")
+
+    class Meta:
+        db_table = 'site_email'
+        verbose_name = 'email'
+        verbose_name_plural = 'emails'
+
+    def __str__(self):
+        return f"Почта: {self.email_name}"
+
+
+class ERIP_link(models.Model):
+    erip_link = models.CharField(max_length=250, blank=True, null=True, verbose_name="Ссылка на ЕРИП")
+
+    class Meta:
+        db_table = 'erip_link'
+        verbose_name = 'ЕРИП ссылки'
+        verbose_name_plural = 'ЕРИП ссылки'
+
+    def __str__(self):
+        return f"ЕРИП: {self.erip_link}"
+
+
+class QRCode(models.Model):
+    qr_name = models.CharField(max_length=100, blank=True, null=True, verbose_name="Название QR кода")
+    qr_picture = models.ImageField(upload_to='qr/', blank=True, null=True, verbose_name="QR код")
+
+    class Meta:
+        db_table = 'qr_code'
+        verbose_name = 'QR код'
+        verbose_name_plural = 'QR коды'
+
+    def __str__(self):
+        return f"QR код: {self.qr_name}"
---
+++ fondmgvv/settings.py
@@ -0,0 +1,113 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG') == 'True'
+
+ALLOWED_HOSTS = []
+DOMAIN = 'domainname.com'
+SITE_NAME = 'Site Name'
+
+if DEBUG:
+    DOMAIN_NAME = 'http://127.0.0.1:8000/'
+else:
+    DOMAIN_NAME = os.getenv('DOMAIN_NAME')
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'app_home.apps.AppHomeConfig'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'fondmgvv.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates']
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'fondmgvv.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+LANGUAGE_CODE = 'ru-RU'
+TIME_ZONE = 'Europe/Moscow'
+
+USE_I18N = True
+USE_I18N = True
+USE_TZ = False
+
+
+STATIC_URL = 'static/'
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static',
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = True
