6406edee3379030cb8960911750ed41068de589f
xumpocmb <x.xumpocmb@gmail.com>
2025-02-20 07:52:17 +0300

    work with app_club

--- static/js/region_filter.js
+++ static/js/region_filter.js
@@ -2,32 +2,40 @@ document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('.clubs__btn');
     const select = document.getElementById('region-select');
     const cards = document.querySelectorAll('.clubs__card');
+    const clubSwiper = document.getElementById('clubSwiper')
 
-    function filterCards(regionId) {
+    function filterCards(regionId, cityId) {
+        console.log(regionId, cityId)
         cards.forEach(card => {
             if (regionId === 'all') {
-                card.style.display = 'block'; // Показываем все карточки
+                card.style.display = 'block';
+            } else if (card.getAttribute('data-city') == cityId) {
+                console.log("фильтр по сити")
+                card.style.display = 'block';
             } else if (card.getAttribute('data-region') === regionId) {
+                console.log("фильтр по регион")
                 card.style.display = 'block';
             } else {
                 card.style.display = 'none';
             }
         });
+        clubSwiper.swiper.slideTo(0);
     }
 
     buttons.forEach(button => {
         button.addEventListener('click', function () {
             const regionId = this.getAttribute('data-region');
-            filterCards(regionId);
+            const cityID = Number(this.getAttribute('data-city'));
+            filterCards(regionId, cityID);
         });
     });
 
     select.addEventListener('change', function () {
         const regionId = this.value;
-        filterCards(regionId);
+        const cityID = this.value;
+        filterCards(regionId, cityID);
     });
 
-    // Показываем все карточки при загрузке страницы
     cards.forEach(card => {
         card.style.display = 'block';
     });
