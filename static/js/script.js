a008c835112d8a97969be23a6a050dc50175e0cf
xumpocmb <x.xumpocmb@gmail.com>
2025-02-21 11:29:36 +0300

    fix styles

--- static/js/script.js
+++ static/js/script.js
@@ -110,3 +110,17 @@ document.addEventListener("DOMContentLoaded", () => {
     isTextVisible = !isTextVisible;
   });
 });
+
+window.addEventListener("DOMContentLoaded", (e) => {
+
+    var IS_IPHONE = navigator.userAgent.match(/iPhone/i) != null;
+    console.log(IS_IPHONE)
+    if (IS_IPHONE) {
+        var link=document.createElement("link");
+        link.type="text/css";
+        link.rel="stylesheet";
+        link.href="../styles/iphone.css";
+        document.getElementsByTagName("head")[0].appendChild(link);
+    }
+
+})
